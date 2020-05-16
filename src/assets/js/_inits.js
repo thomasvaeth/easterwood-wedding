a3158dc2907427feade5f0028d8aeb74f50c47e9
thomasvaeth <thomas.vaeth@gmail.com>
2020-05-16 16:11:30 -0700

    Compressed photo.

--- src/assets/js/_inits.js
+++ src/assets/js/_inits.js
@@ -7,18 +7,4 @@
 // ----------------------------------------------
 $(() => {
 
-  const count = $('.images').children().length;
-  let random;
-
-  $('.overlay__subcontainer').mouseenter(() => {
-    $('.images').children().removeClass('js-visible');
-
-    random = Math.floor(Math.random() * count);
-
-    $('.images').children().eq(random).addClass('js-visible');
-  });
-
-  // $('.overlay__subcontainer').mouseleave(() => {
-  //   $('.images').children().eq(random).removeClass('js-visible');
-  // });
 });
