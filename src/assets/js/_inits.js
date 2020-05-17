189de49f133ee246d4161a78b326c8646a75ff14
thomasvaeth <thomas.vaeth@gmail.com>
2020-05-17 07:54:37 -0700

    Fixed IO on mobile.

--- src/assets/js/_inits.js
+++ src/assets/js/_inits.js
@@ -12,12 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0,
+      threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
     };
 
     function handleIntersect(entries, observer) {
       entries.forEach(entry => {
-        if (entry.isIntersecting) {
+        if (entry.isIntersecting && entry.intersectionRatio > 0.1) {
           entry.target.classList.add('fade-in');
         }
       });
