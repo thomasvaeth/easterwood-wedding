4f228cc4d6b953fbc3231e00b084a6220a7e6bd6
thomasvaeth <thomas.vaeth@gmail.com>
2020-05-16 22:16:15 -0700

    Added scrolling animations.

--- src/assets/js/_inits.js
+++ src/assets/js/_inits.js
@@ -5,6 +5,32 @@
 // ----------------------------------------------
 // Inits
 // ----------------------------------------------
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
+  const observables = document.querySelectorAll('img');
 
+  if ('IntersectionObserver' in window) {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    };
+
+    function handleIntersect(entries, observer) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-in');
+        }
+      });
+    }
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+
+    observables.forEach(observable => {
+      observer.observe(observable);
+    });
+  } else {
+    observables.forEach(observable => {
+      observable.classList.add('fade-in');
+    });
+  }
 });
